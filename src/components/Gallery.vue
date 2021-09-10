2ad4dd6124e38de1e8daa92edb049aae077158ef
Alaa <alaadawabah@gmail.com>
2021-09-10 18:57:21 +0200

    configure store and integrate with API

---
+++ src/components/Gallery.vue
@@ -0,0 +1,22 @@
+<template lang="html">
+  <div v-if="photos.length">
+    <div v-for="photo in photos" :key="photo.id">
+        <img :src={{photo.url}}/>
+    </div>
+  </div>
+  <div v-else>Loading ...</div>
+</template>
+<script>
+export default {
+  name: "Gallery",
+  computed: {
+    photos() {
+      return this.$store.getters.photos;
+    },
+  },
+  mounted() {
+    this.$store.dispatch("getPhotos");
+  },
+};
+</script>
+<style lang="css"></style>
---
+++ src/store/store.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+import { createStore } from "vuex";
+
+export const store = createStore({
+  state: {
+    photos: [],
+  },
+  mutations: {
+    photosReceived: (state, payload) => {
+      console.log("from received", payload);
+      return (state.photos = payload);
+    },
+  },
+  actions: {
+    getPhotos: (context) => {
+      console.log("context", context);
+      axios
+        .get(`https://jsonplaceholder.typicode.com/photos`)
+        .then((res) => {
+          console.log("res", res);
+          context.commit("photosReceived", res.data.splice(0, 49));
+        })
+        .catch((err) => console.log("err", err));
+    },
+  },
+  getters: {
+    photos: (state) => state.photos,
+  },
+});
